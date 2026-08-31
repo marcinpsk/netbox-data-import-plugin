3cabf2934d9c1da1e046828a82a9e95a60f3d0b9
semantic-release <semantic-release>
2026-08-31 19:05:17 +0000

    chore(release): 1.11.0
    Automatically generated by python-semantic-release

--- netbox_data_import/__init__.py
+++ netbox_data_import/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2025 Marcin Zieba <marcinpsk@gmail.com>
 from netbox.plugins import PluginConfig
 
-__version__ = "1.10.0"
+__version__ = "1.11.0"
 
 
 class NetBoxDataImportConfig(PluginConfig):
